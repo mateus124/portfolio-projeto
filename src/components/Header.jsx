4c69fe345e283262f9d0853b78c358c68e85fe44
Mateus Alves <107895139+mateus124@users.noreply.github.com>
2024-10-21 20:02:04 -0300

    responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,19 @@
 import styles from './Header.module.css'
 import logo from '../assets/logo-horizontal.png'
+import { useState } from 'react';
 
 function Header(props) {
+    const [isActive, setIsActive] = useState(false);
+    const toggleClass = () => {
+        setIsActive(!isActive);
+    };
+    
     return (
         <nav className={`${styles.header} ${styles[localStorage.getItem('theme')]}`}>
             <div className={styles.logo}>
                 <img src={logo} alt="logo devindev"/>
             </div>
-            <ul className={styles.list}>
+            <ul className={`${styles.navMenu} ${isActive ? styles.active : 'inactive'}`}>
                 <li>Sobre mim</li>
                 <li>Meus projetos</li>
                 <li>Habilidades</li>
@@ -16,6 +22,11 @@ function Header(props) {
                     <input type="checkbox" onChange={() => props.mudarTema()} />
                 </li>
             </ul>
+            <div className={`${styles.hamb} ${isActive ? styles.active : 'inactive'}`} onClick={toggleClass}>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
         </nav>
     )
 }
